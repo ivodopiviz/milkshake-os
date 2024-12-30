bc3b2b3d8c45ba4b9cb9d69a761a9479167c33c0
Ivan Vodopiviz <ivanvodo@gmail.com>
2024-12-30 17:54:27 +0100

    Another binding try

--- scripts/build.sh
+++ scripts/build.sh
@@ -46,7 +46,7 @@ function find_index() {
 function chroot_enter_setup() {
     sudo mount --bind /dev chroot/dev
     sudo mount --bind /run chroot/run
-    sudo mount --bind / chroot/
+    sudo mount --bind / chroot
 
     sudo chroot chroot mount none -t proc /proc
     sudo chroot chroot mount none -t sysfs /sys
@@ -59,7 +59,7 @@ function chroot_exit_teardown() {
     sudo chroot chroot umount /dev/pts
     sudo umount chroot/dev
     sudo umount chroot/run
-    sudo umount chroot/
+    sudo umount chroot
 }
 
 function check_host() {
